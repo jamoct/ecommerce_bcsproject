8398cd8267a4ecd66fe44161fbba131bccae1cf2
joyce4 <joyce@barcelonacodeschool.com>
2019-09-06 19:33:14 +0200

    still trying to fix

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// =============== DATABASE CONNECTION =====================
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || `mongodb://heroku_hcjfblpk:t9h6166a2t675uk69tm76albkg@ds317808.mlab.com:17808/heroku_hcjfblpk`,  { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
 // =============== ROUTES ==============================
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
@@ -25,17 +30,12 @@ app.use('/', productRoutes);
 app.use('/', cartRoutes);
 app.use('/', orderRoutes);
 
-// =============== DATABASE CONNECTION =====================
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://heroku_hcjfblpk:t9h6166a2t675uk69tm76albkg@ds317808.mlab.com:17808/heroku_hcjfblpk`,  { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
-
 // to make the project production ready
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   const path = require('path');
-  app.get('/', (req,res) => {
+  app.get('*', (req,res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 
